3642a2d4358e36bf5a48a9cd41fff72c416d3531
Beth Hart <errordrivendev@gmail.com>
2018-08-25 17:29:08 -0700

    Add project files.

---
+++ PflStoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PflStoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
